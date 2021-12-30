31aa5431c05c7e902bde6ea69e17995499347d4c
Manny F <mfls@pm.me>
2021-12-30 11:03:27 -0500

    created playRound function

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
 function computerPlay() {
     var optionList = ["rock", "paper", "scissors"]
-    var option = [optionList Math.floor(Math.random() * 3)]
-    return option 
+    var option = [optionList, Math.floor(Math.random() * 3)]
+    return option; 
 }
 
+function playRound (playerSelection, computerSelection) {
+    var playerSelection = prompt("What is your choice? Rock, Paper, or Scissors?").toLowerCase();
+    var computerSelection = computerPlay;
+       
+        if (playerSelection === "rock") {
+            switch (computerSelection) {
+                case "rock":
+                return ("tie");
+                break;
+
+                case "paper":
+                return ("win");
+                break;
+
+                case "scissors":
+                return ("lose");
+                break;
+            }
+        }
+        else if (playerSelection === "paper") {
+            switch (computerSelection) {
+                case "rock":
+                return ("lose");
+                break;
+
+                case "paper":
+                return ("tie");
+                break;
+
+                case "scissors":
+                return ("win");
+                break;
+            }
+        }
+        else if (playerSelection === "scissors") {
+            switch (computerSelection) {
+                case "rock":
+                return ("win");
+                break;
+
+                case "paper":
+                return ("lose");
+                break;
+
+                case "scissors":
+                return ("tie");
+                break;
+        }
+    }
+}
